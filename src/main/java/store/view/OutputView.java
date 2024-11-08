49d98d9bcb9328ec54a0a20db13bfff3fea3727c
gikhoon <ghikhoon@naver.com>
2024-11-09 03:55:49 +0900

    feat: 에러 메세지 출력 메소드 구현

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -5,11 +5,17 @@ import static store.constant.ViewMessageConstant.WELCOME_MESSAGE;
 import store.controller.dto.ProductInfoDto;
 
 public class OutputView {
+    private static final String ERROR_PREFIX = "[ERROR] ";
+
     public void welcomeMessage() {
         System.out.println(WELCOME_MESSAGE);
     }
 
     public void printProducts(ProductInfoDto productInfos) {
         productInfos.productInfo().forEach(System.out::println);
+    }
+
+    public void printErrorMessage(String message) {
+        System.out.println(ERROR_PREFIX + message);
     }
 }
