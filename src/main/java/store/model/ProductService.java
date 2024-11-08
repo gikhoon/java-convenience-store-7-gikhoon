e4b64244854e024a479d52aab33ff172bcc3ba4b
gikhoon <ghikhoon@naver.com>
2024-11-09 03:24:04 +0900

    feat: static 메소드로 ProductInfoDto 생성

--- src/main/java/store/model/ProductService.java
+++ src/main/java/store/model/ProductService.java
@@ -1,18 +1,15 @@
 package store.model;
 
 import java.util.List;
-import store.controller.dto.ProductInfo;
 import store.controller.dto.ProductInfoDto;
+import store.model.entity.Product;
 import store.model.repository.ProductRepository;
 
 public class ProductService {
     private static final ProductRepository productRepository = new ProductRepository();
 
     public ProductInfoDto getAllProductInfos() {
-        List<ProductInfo> productInfos = productRepository.findAll().stream()
-                .map(ProductInfo::from)
-                .toList();
-
-        return new ProductInfoDto(productInfos);
+        List<Product> products = productRepository.findAll();
+        return ProductInfoDto.toDto(products);
     }
 }
