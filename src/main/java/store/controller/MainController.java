67396f891d2559553623e3a2015e957e8086e23b
gikhoon <ghikhoon@naver.com>
2024-11-10 02:42:53 +0900

    feat: 재구매 선택 시 처음으로 돌아오게 설계

--- src/main/java/store/controller/MainController.java
+++ src/main/java/store/controller/MainController.java
@@ -2,19 +2,29 @@ package store.controller;
 
 import store.controller.dto.ProductOrderList;
 import store.model.ProductService;
+import store.util.YesOrNoParser;
+import store.view.InputView;
 import store.view.OutputView;
 
 public class MainController {
     private final InitConvenienceStoreController initController = new InitConvenienceStoreController();
     private final ProductOrderController productOrderController = new ProductOrderController();
     private final ProductService productService = new ProductService();
+    private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
 
     public void run() {
         initController.initPromotions();
         initController.initProducts();
-        printProductInfo();
-        ProductOrderList buyProducts = productOrderController.orderProduct();
+        buy();
+    }
+
+    private void buy() {
+        do {
+            printProductInfo();
+            ProductOrderList buyProducts = productOrderController.orderProduct();
+            outputView.printReorder();
+        } while (YesOrNoParser.parseYesOrNo(inputView.inputYesOrNo()));
     }
 
     private void printProductInfo() {
