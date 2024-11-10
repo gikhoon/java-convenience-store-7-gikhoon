caf0941baf197c11c3528174ecabbf09b7aa6c60
gikhoon <ghikhoon@naver.com>
2024-11-10 14:17:58 +0900

    refactor: 영수증 객체 record로 변경

--- src/main/java/store/controller/dto/ReceiptDto.java
+++ src/main/java/store/controller/dto/ReceiptDto.java
@@ -1,13 +1,8 @@
 package store.controller.dto;
 
-public class ReceiptDto {
-    private OrderProduct orderProduct;
-    private PromotionProduct promotionProduct;
-    private PriceData priceData;
-
-    public ReceiptDto(OrderProduct orderProduct, PromotionProduct promotionProduct, PriceData priceData) {
-        this.orderProduct = orderProduct;
-        this.promotionProduct = promotionProduct;
-        this.priceData = priceData;
-    }
+public record ReceiptDto(
+        OrderProduct orderProduct,
+        PromotionProduct promotionProduct,
+        PriceData priceData
+) {
 }
